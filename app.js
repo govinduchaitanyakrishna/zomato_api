beb977942b45a6307675fb73005c2a1e2caf422f
“GitHub_user_name_here” <govinduchaitanya3@gmail.com>
2021-12-06 16:54:44 +0530

    code

--- app.js
+++ app.js
@@ -117,7 +117,8 @@ app.get('/menu/:id',(req,res) => {
 })
 
 app.post('/menuItem',(req,res) => {
-    db.collection('menu').find({menu_id:{$in:req.body.ids}}).toArray((err,result)=>{
+    console.log(req.body)
+    db.collection('menu').find({menu_id:{$in:req.body}}).toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
