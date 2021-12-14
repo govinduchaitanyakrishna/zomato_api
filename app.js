aa6830b2475a0c8a835936f0b8b3cbdf1d7df566
govinduchaitanyakrishna <govinduchaitanya3@gmail.com>
2021-12-14 17:56:11 +0530

    code

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors())
 
-//const mongourl = "mongodb://localhost:27017"
-const mongourl = "mongodb+srv://local:testuser@cluster0.f8vmc.mongodb.net/zomato?retryWrites=true&w=majority"
+const mongourl = "mongodb+srv://govinduchaitanya3:1234@cluster0.jmt1h.mongodb.net/zomato?retryWrites=true&w=majority"
 var db;
 //get
 app.get('/',(req,res) => {
